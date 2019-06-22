1dd8fe1ef0cb1d45ec82f541c9e365a6222be546
tavisca-sniyazi <sniyazi@tavisca.in>
2019-06-22 22:31:49 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,7 +23,155 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
-            throw new NotImplementedException();
+           
+            string[] digits = equation.Split(new char[] { '*','=' });
+
+            if(digits[0].Contains('?'))
+            {
+                int b = Int32.Parse(digits[1]);
+                int c = Int32.Parse(digits[2]);
+
+                if(c%b!=0)
+                {
+                    return (-1);
+                }
+
+                int a = c / b;
+                string x = a.ToString();
+                int index = 0;
+                int same = 0;
+                string A = digits[0];
+
+                if(x.Length==A.Length)
+                {
+                    for(int i=0;i<x.Length;i++ )
+                    {
+                        if(A[i]=='?')
+                        {
+                            index = i; continue;
+                        }
+                        if(A[i]==x[i])
+                        {
+                            same = same + 1;
+
+                        }
+                    }
+
+ 
+                }
+
+                else
+                {
+                    return (-1);
+                }
+                if(same==x.Length-1)
+                {
+                    string y = x[index].ToString();
+                    int z = Int32.Parse(y);
+                    return (z);
+                }
+                else
+                {
+                    return (-1);
+                }
+            }
+
+
+            if (digits[1].Contains('?'))
+            {
+                int a = Int32.Parse(digits[0]);
+                int c = Int32.Parse(digits[2]);
+
+                if (c % a != 0)
+                {
+                    return (-1);
+                }
+
+                int b = c / a;
+                string x = b.ToString();
+                int index = 0;
+                int same = 0;
+                string A = digits[1];
+
+                if (x.Length == A.Length)
+                {
+                    for (int i = 0; i < x.Length; i++)
+                    {
+                        if (A[i] == '?')
+                        {
+                            index = i; continue;
+                        }
+                        if (A[i] == x[i])
+                        {
+                            same = same + 1;
+
+                        }
+                    }
+
+
+                }
+
+                else
+                {
+                    return (-1);
+                }
+                if (same == x.Length - 1)
+                {
+                    string y = x[index].ToString();
+                    int z = Int32.Parse(y);
+                    return (z);
+                }
+                else
+                {
+                    return (-1);
+                }
+            }
+
+            if (digits[2].Contains('?'))
+            {
+                int a = Int32.Parse(digits[0]);
+                int b = Int32.Parse(digits[1]);
+
+                int c = a * b;
+                string x = c.ToString();
+                int index = 0;
+                int same = 0;
+                string A = digits[2];
+
+                if (x.Length == A.Length)
+                {
+                    for (int i = 0; i < x.Length; i++)
+                    {
+                        if (A[i] == '?')
+                        {
+                            index = i; continue;
+                        }
+                        if (A[i] == x[i])
+                        {
+                            same = same + 1;
+
+                        }
+                    }
+
+
+                }
+
+                else
+                {
+                    return (-1);
+                }
+                if (same == x.Length - 1)
+                {
+                    string y = x[index].ToString();
+                    int z = Int32.Parse(y);
+                    return (z);
+                }
+                else
+                {
+                    return (-1);
+                }
+            }
+            return 0;
         }
     }
 }
